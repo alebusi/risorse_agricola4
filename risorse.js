e3cb000823a0980600f24ceee83141d428ceca0b
alebusi <allebusi@gmail.com>
2018-10-23 11:52:25 +0200

    Update risorse.js

--- risorse.js
+++ risorse.js
@@ -2,11 +2,21 @@ var tess = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 /*var success = new Audio('success.wav');*/
 
 function myfunc(id,i,ind) {
-  tess[ind]=0;
-  v=tess[ind];
-  document.getElementById(id).innerHTML = v;
-  var suona = new Audio('togli.mp3');
-  suona.play();
+  if (tess[6] == 0)
+    {
+    tess[ind]=0;
+    v=tess[ind];
+    document.getElementById(id).innerHTML = v;
+    var suona = new Audio('togli.mp3');
+    suona.play();
+    }
+  else
+    {
+    tess[ind]+=1;
+    document.getElementById(id).innerHTML = v;
+    var suona = new Audio('success.wav');
+    suona.play();
+    }
 }
 
 function myfunc2(id,i,ind) {
@@ -23,6 +33,21 @@ function myfunc2(id,i,ind) {
   document.getElementById("legno2").innerHTML = tess[3];
   document.getElementById("argilla1").innerHTML = tess[4];
   document.getElementById("cibo").innerHTML = tess[5];  
+  var suona = new Audio('success.wav');
+  suona.play();
+}
+
+function myfunc3(div) {
+  if ( tess[6] == 0 )
+    {
+    tess[6] == 1;
+    div.className = "on";
+    }
+  else
+    {
+    tess[6] == 0;
+    div.className = "off";
+    }
   var suona = new Audio('success.wav');
   suona.play();
 }
